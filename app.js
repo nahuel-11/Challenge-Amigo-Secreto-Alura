38449b09760de1d2143a9acf79ae908e7a897f51
nahuel-11 <nahuelromano911@gmail.com>
2025-01-24 17:46:39 -0300

    se creo la funcion para elegir un nombre aleatorio del array de amigos

--- app.js
+++ app.js
@@ -15,8 +15,19 @@ function agregarAmigo() {
 }
 
 function mostrarListaAmigos(ulAmigos) {
-    let elemetoHTML = document.getElementById("listaAmigos");
-    elemetoHTML.innerHTML = "";
-    let nombres = ulAmigos.join(", ");
-    elemetoHTML.textContent = nombres;
+  let elemetoHTML = document.getElementById("listaAmigos");
+  elemetoHTML.innerHTML = "";
+  let nombres = ulAmigos.join(", ");
+  elemetoHTML.textContent = nombres;
+}
+
+function sortearAmigo() {
+  if (listaAmigoss.length === 0) {
+    alert("Debe agregar un amigo antes de sortear");
+    return;
   }
+
+  let indiceAleatorio = Math.floor(Math.random() * listaAmigoss.length);
+  let amigoSeleccionado = listaAmigoss[indiceAleatorio];
+  mostrarResultado(amigoSeleccionado);
+}
